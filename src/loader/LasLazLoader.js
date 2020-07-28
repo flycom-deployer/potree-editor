b5917f4ad390fd5d012db890819bd19761894c19
FlycomTechnologies <15048113+flycom-deployer@users.noreply.github.com>
2020-07-28 08:40:51 +0200

    Update LasLazLoader.js
    Updated to pass original pointFormatId.

--- src/loader/LasLazLoader.js
+++ src/loader/LasLazLoader.js
@@ -1,7 +1,5 @@
-
-
-import {Version} from "../Version.js";
-import {XHRFactory} from "../XHRFactory.js";
+import { Version } from '../Version.js';
+import { XHRFactory } from '../XHRFactory.js';
 
 /**
  * laslaz code taken and adapted from plas.io js-laslaz
@@ -13,8 +11,7 @@ import {XHRFactory} from "../XHRFactory.js";
  */
 
 export class LasLazLoader {
-
-	constructor (version, extension) {
+	constructor(version, extension) {
 		if (typeof (version) === 'string') {
 			this.version = new Version(version);
 		} else {
@@ -24,11 +21,11 @@ export class LasLazLoader {
 		this.extension = extension;
 	}
 
-	static progressCB () {
+	static progressCB() {
 
 	}
 
-	load (node) {
+	load(node) {
 		if (node.loaded) {
 			return;
 		}
@@ -39,17 +36,17 @@ export class LasLazLoader {
 			url += `.${this.extension}`;
 		}
 
-		let xhr = XHRFactory.createXMLHttpRequest();
+		const xhr = XHRFactory.createXMLHttpRequest();
 		xhr.open('GET', url, true);
 		xhr.responseType = 'arraybuffer';
 		xhr.overrideMimeType('text/plain; charset=x-user-defined');
 		xhr.onreadystatechange = () => {
 			if (xhr.readyState === 4) {
 				if (xhr.status === 200 || xhr.status === 0) {
-					let buffer = xhr.response;
+					const buffer = xhr.response;
 					this.parse(node, buffer);
 				} else {
-					console.log('Failed to load file! HTTP status: ' + xhr.status + ', file: ' + url);
+					console.log(`Failed to load file! HTTP status: ${xhr.status}, file: ${url}`);
 				}
 			}
 		};
@@ -57,29 +54,29 @@ export class LasLazLoader {
 		xhr.send(null);
 	}
 
-	async parse(node, buffer){
-		let lf = new LASFile(buffer);
-		let handler = new LasLazBatcher(node);
+	async parse(node, buffer) {
+		const lf = new LASFile(buffer);
+		const handler = new LasLazBatcher(node);
 
-		try{
+		try {
 			 await lf.open();
 			 lf.isOpen = true;
-		}catch(e){
-			console.log("failed to open file. :(");
+		} catch (e) {
+			console.log('failed to open file. :(');
 
 			return;
 		}
 
-		let header = await lf.getHeader();
+		const header = await lf.getHeader();
 
-		let skip = 1;
+		const skip = 1;
 		let totalRead = 0;
-		let totalToRead = (skip <= 1 ? header.pointsCount : header.pointsCount / skip);
+		const totalToRead = (skip <= 1 ? header.pointsCount : header.pointsCount / skip);
 
 		let hasMoreData = true;
 
-		while(hasMoreData){
-			let data = await lf.readData(1000 * 1000, 0, skip);
+		while (hasMoreData) {
+			const data = await lf.readData(1000 * 1000, 0, skip);
 
 			handler.push(new LASDecoder(data.buffer,
 				header.pointsFormatId,
@@ -101,46 +98,45 @@ export class LasLazLoader {
 
 		LasLazLoader.progressCB(1);
 
-		try{
+		try {
 			await lf.close();
 
 			lf.isOpen = false;
-		}catch(e){
-			console.error("failed to close las/laz file!!!");
-			
+		} catch (e) {
+			console.error('failed to close las/laz file!!!');
+
 			throw e;
 		}
 	}
 
-	handle (node, url) {
+	handle(node, url) {
 
 	}
-};
+}
 
-export class LasLazBatcher{
-
-	constructor (node) {
+export class LasLazBatcher {
+	constructor(node) {
 		this.node = node;
 	}
 
-	push (lasBuffer) {
-		const workerPath = Potree.scriptPath + '/workers/LASDecoderWorker.js';
+	push(lasBuffer) {
+		const workerPath = `${Potree.scriptPath}/workers/LASDecoderWorker.js`;
 		const worker = Potree.workerPool.getWorker(workerPath);
-		const node = this.node;
-		const pointAttributes = node.pcoGeometry.pointAttributes;
+		const { node } = this;
+		const { pointAttributes } = node.pcoGeometry;
 
-		worker.onmessage = (e) => {
-			let geometry = new THREE.BufferGeometry();
-			let numPoints = lasBuffer.pointsCount;
+		worker.onmessage = e => {
+			const geometry = new THREE.BufferGeometry();
+			const numPoints = lasBuffer.pointsCount;
 
-			let positions = new Float32Array(e.data.position);
-			let colors = new Uint8Array(e.data.color);
-			let intensities = new Float32Array(e.data.intensity);
-			let classifications = new Uint8Array(e.data.classification);
-			let returnNumbers = new Uint8Array(e.data.returnNumber);
-			let numberOfReturns = new Uint8Array(e.data.numberOfReturns);
-			let pointSourceIDs = new Uint16Array(e.data.pointSourceID);
-			let indices = new Uint8Array(e.data.indices);
+			const positions = new Float32Array(e.data.position);
+			const colors = new Uint8Array(e.data.color);
+			const intensities = new Float32Array(e.data.intensity);
+			const classifications = new Uint8Array(e.data.classification);
+			const returnNumbers = new Uint8Array(e.data.returnNumber);
+			const numberOfReturns = new Uint8Array(e.data.numberOfReturns);
+			const pointSourceIDs = new Uint16Array(e.data.pointSourceID);
+			const indices = new Uint8Array(e.data.indices);
 
 			geometry.addAttribute('position', new THREE.BufferAttribute(positions, 3));
 			geometry.addAttribute('color', new THREE.BufferAttribute(colors, 4, true));
@@ -152,7 +148,7 @@ export class LasLazBatcher{
 			geometry.addAttribute('indices', new THREE.BufferAttribute(indices, 4));
 			geometry.attributes.indices.normalized = true;
 
-			for(const key in e.data.ranges){
+			for (const key in e.data.ranges) {
 				const range = e.data.ranges[key];
 
 				const attribute = pointAttributes.attributes.find(a => a.name === key);
@@ -160,9 +156,9 @@ export class LasLazBatcher{
 				attribute.range[1] = Math.max(attribute.range[1], range[1]);
 			}
 
-			let tightBoundingBox = new THREE.Box3(
+			const tightBoundingBox = new THREE.Box3(
 				new THREE.Vector3().fromArray(e.data.tightBoundingBox.min),
-				new THREE.Vector3().fromArray(e.data.tightBoundingBox.max)
+				new THREE.Vector3().fromArray(e.data.tightBoundingBox.max),
 			);
 
 			geometry.boundingBox = this.node.boundingBox;
@@ -178,16 +174,17 @@ export class LasLazBatcher{
 			Potree.workerPool.returnWorker(workerPath, worker);
 		};
 
-		let message = {
+		const message = {
 			buffer: lasBuffer.arrayb,
 			numPoints: lasBuffer.pointsCount,
 			pointSize: lasBuffer.pointSize,
-			pointFormatID: 2,
+			// pointFormatID: 2,
+			pointFormatID: parseInt(lasBuffer.decoder.name),
 			scale: lasBuffer.scale,
 			offset: lasBuffer.offset,
 			mins: lasBuffer.mins,
-			maxs: lasBuffer.maxs
+			maxs: lasBuffer.maxs,
 		};
 		worker.postMessage(message, [message.buffer]);
-	};
+	}
 }
